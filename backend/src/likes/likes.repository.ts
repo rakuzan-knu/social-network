2fe80fbf3e8e0322f796ec0f8baea628f314bdd3
subvincdebian <aghmichal68@gmail.com>
2026-07-04 19:13:42 +0300

    fix(infra): fix type imports, missing await and DI decorator [SOC-8]

--- backend/src/likes/likes.repository.ts
+++ backend/src/likes/likes.repository.ts
@@ -1,22 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { ILikesRepository } from './likes-repository.interface';
+import type { ILikesRepository } from './likes-repository.interface';
 import { PrismaService } from '../prisma/prisma.service';
-import { Like } from '@prisma/client';
+import type { Like } from '@prisma/client';
 
 @Injectable()
 export class LikesRepository implements ILikesRepository {
   constructor(private readonly prismaService: PrismaService) {}
-
   async createLike(postId: string, userId: string): Promise<Like> {
     return this.prismaService.like.create({
-      data: { postId, userId },
+      data: {
+        postId,
+        userId,
+      },
     });
   }
-
   async deleteLike(postId: string, userId: string): Promise<void> {
     await this.prismaService.like.delete({
       where: {
-        postId_userId: { postId, userId },
+        postId_userId: {
+          postId,
+          userId,
+        },
       },
     });
   }
